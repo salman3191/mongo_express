501fdd662530227c80791365a3c96756dd693774
salman <mohammadsalmankhan3191@gmail.com>
2025-09-13 20:12:16 +0530

    this is how we create index route for all chats

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 main()
   .then(() => {
@@ -18,21 +19,26 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
-let chat1 = new chat({
-  from: "zennie",
-  to: "salman",
-  msg: "i love u more my jaan",
-  created_at: new Date(),
-});
-chat1
-  .save()
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// let chat1 = new chat({
+//   from: "zennie",
+//   to: "salman",
+//   msg: "i love u more my jaan",
+//   created_at: new Date(),
+// });
+// chat1
+//   .save()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
+app.get("/chats", async (req, res) => {
+  let chats = await chat.find();
+  res.render("index.ejs", { chats });
+  //   console.log(chats);
+});
 app.get("/", (req, res) => {
   res.send("server working well");
 });
